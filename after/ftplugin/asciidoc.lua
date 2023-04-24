a2bb17abd9937b43cd228a8e3c8f3fc07c931d31
Keith W. Davis <kwdprof@gmail.com>
2023-04-24 10:43:58 -0400

    fixed a conflict I did not notice in asdciidoc after file.

--- after/ftplugin/asciidoc.lua
+++ after/ftplugin/asciidoc.lua
@@ -1,12 +1,5 @@
 vim.opt.linebreak = true
-<<<<<<< HEAD
 vim.g.asciidoctor_backend = "html5s"
 vim.g.asciidoctor_extensions = { "asciidoctor-html5s" }
 vim.cmd("SoftWrapMode")
-
 vim.cmd.colorscheme("habamax")
-=======
-vim.cmd("SoftWrapMode")
-vim.g.asciidoctor_extensions = { "asciidoctor-html5s" }
-vim.g.asciidoctor_backend = "html5s"
->>>>>>> ecdc6a4bea697a5889abc334f242ca5dbf3f2af6
